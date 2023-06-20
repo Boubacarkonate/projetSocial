01133fed4bee70ed2349398f98c5cbaac4f82842
Bouba <boubacar.konate@outlook.fr>
2023-06-20 13:48:44 +0200

    création entité Categorie

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sector = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSector(): ?string
+    {
+        return $this->sector;
+    }
+
+    public function setSector(string $sector): static
+    {
+        $this->sector = $sector;
+
+        return $this;
+    }
+}
