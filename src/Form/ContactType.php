14110663155294cf2b8f0913bd09b3f16c5d2170
Bouba <boubacar.konate@outlook.fr>
2023-06-27 21:24:53 +0200

    site ok + responive en cours

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -15,12 +15,36 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('name',TextType::class)
-        ->add('firstname', TextType::class) 
-        ->add('email',EmailType::class)
-        ->add('telephone', TextType::class)
-        ->add('objet', TextType::class)
-        ->add('message', TextareaType::class) 
+        ->add('name',TextType::class, [
+            'label' => "Nom", 
+           'attr' => [
+               'class'=> 'form-control-lg'],
+            ])
+        ->add('firstname', TextType::class, [
+            'label' => "Prénom", 
+           'attr' => [
+               'class'=> 'form-control-lg'],
+            ]) 
+        ->add('email',EmailType::class, [
+            'label' => "Email", 
+           'attr' => [
+               'class'=> 'form-control-lg'],
+            ])
+        ->add('telephone', TextType::class, [
+            'label' => "Téléphone", 
+           'attr' => [
+               'class'=> 'form-control-lg'],
+            ])
+        ->add('objet', TextType::class, [
+            'label' => "Objet du message", 
+           'attr' => [
+               'class'=> 'form-control-lg'],
+            ])
+        ->add('message', TextareaType::class, [
+            'label' => "Message", 
+           'attr' => [
+               'class'=> 'form-control-lg'],
+            ]) 
       
         ->add('Envoyer', SubmitType::class)
         ;
